018c82868e3768178ee35d239ba2b4ad9750310c
MauriKlein <maurijklein@gmail.com>
2022-12-03 10:06:48 -0300

    Incluindo documentação Swagger

--- src/main/java/com/dh/meli/exemplo_consumo_api/controller/CepController.java
+++ src/main/java/com/dh/meli/exemplo_consumo_api/controller/CepController.java
@@ -3,10 +3,7 @@ package com.dh.meli.exemplo_consumo_api.controller;
 import com.dh.meli.exemplo_consumo_api.dto.CepDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -16,8 +13,12 @@ public class CepController {
     @GetMapping("consultaCep/{cep}")
     public ResponseEntity<CepDto> retornaEndereco(@PathVariable String cep){
         CepDto cepDto = new RestTemplate().getForEntity("https://viacep.com.br/ws/"+cep+"/json/", CepDto.class).getBody();
-
         return new ResponseEntity(cepDto, HttpStatus.OK);
+    }
+
+    @PostMapping("salvar")
+    public CepDto salvarCep(@RequestBody CepDto cepDto){
+        return cepDto;
     }
 
 }
--- src/main/java/com/dh/meli/exemplo_consumo_api/dto/CepDto.java
+++ src/main/java/com/dh/meli/exemplo_consumo_api/dto/CepDto.java
@@ -16,9 +16,4 @@ public class CepDto implements Serializable {
     private String bairro;
     private String localidade;
     private String uf;
-    private String ibge;
-    private String gia;
-    private String ddd;
-    private String siafi;
-
 }
